34bda6112028a6a184926071200bc7961e182f04
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-11 16:24:54 +0430

    add dependency for show music file in andorid and Ios

--- lib/views/playorstopsong.dart
+++ lib/views/playorstopsong.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:nightplayer/module/constans.dart';
-import 'package:nightplayer/module/widgets.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 
+import '../module/constans.dart';
 import '../module/extention.dart';
+import '../module/widgets.dart';
 
 class PlayOrStopSong extends StatelessWidget {
   const PlayOrStopSong({Key? key}) : super(key: key);
@@ -22,125 +22,132 @@ class PlayOrStopSong extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
               flex: 1,
               child: Container(
-                  padding: const EdgeInsets.only(left: 30, right: 30),
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    right: 30,
+                  ),
                   clipBehavior: Clip.hardEdge,
                   decoration:
                       BoxDecoration(borderRadius: BorderRadius.circular(40)),
-                  child: Image.asset('assets/img/kaj.jpg'))),
-          const SizedBox(
-            height: 30,
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              width: double.infinity,
-              height: double.infinity,
-              decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(35),
-                      topRight: Radius.circular(35))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 30, left: 30, right: 30),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "Kaj",
-                          style: context.textTheme.subtitle1!
-                              .copyWith(fontSize: 28),
-                        ),
-                        const Spacer(),
-                        Container(
-                          width: 70,
-                          height: 70,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.white.withAlpha(30)),
-                          child: const Center(
-                            child: Icon(
-                              Icons.favorite,
-                              color: Colors.red,
-                              size: 40,
+                  child: Image.asset('assets/img/kaj.jpg')),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(35),
+                        topRight: Radius.circular(35))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 20, left: 30, right: 30),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Kaj",
+                            style: context.textTheme.subtitle1!
+                                .copyWith(fontSize: 26),
+                          ),
+                          const Spacer(),
+                          Container(
+                            width: 65,
+                            height: 65,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white.withAlpha(30)),
+                            child: const Center(
+                              child: Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                                size: 40,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    child: Text(
-                      "Mehrad Hidden",
-                      style: context.textTheme.subtitle1!.copyWith(
-                          fontSize: 20, fontWeight: FontWeight.normal),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
+                      child: Text(
+                        "Mehrad Hidden",
+                        style: context.textTheme.subtitle1!.copyWith(
+                            fontSize: 20, fontWeight: FontWeight.normal),
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 30),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: LinearPercentIndicator(
-                        width: MediaQuery.of(context).size.width - 42,
-                        animation: true,
-                        lineHeight: 15.0,
-                        barRadius: const Radius.circular(8),
-                        animationDuration: 1000,
-                        percent: 0.5,
-                        backgroundColor: Constans.kwhite.withAlpha(30),
-                        progressColor: Colors.purple),
-                  ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    child: Row(
-                      children: [
-                        Text(
-                          "2:13",
-                          style: context.textTheme.subtitle1,
-                        ),
-                        const Spacer(),
-                        Text(
-                          "4:10",
-                          style: context.textTheme.subtitle1,
-                        ),
-                      ],
+                    const SizedBox(height: 25),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: LinearPercentIndicator(
+                          width: MediaQuery.of(context).size.width - 42,
+                          animation: true,
+                          lineHeight: 15.0,
+                          barRadius: const Radius.circular(8),
+                          animationDuration: 1000,
+                          percent: 0.5,
+                          backgroundColor: Constans.kwhite.withAlpha(30),
+                          progressColor: Colors.purple),
                     ),
-                  ),
-                  const SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 30, right: 30),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        MButtonForSong(
-                          icon: Icons.arrow_back_ios,
-                        ),
-                        const SizedBox(width: 20),
-                        MButtonForSong(
-                          icon: Icons.play_arrow,
-                          boxColor: true,
-                        ),
-                        SizedBox(width: 20),
-                        MButtonForSong(icon: Icons.arrow_forward_ios)
-                      ],
+                    const SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
+                      child: Row(
+                        children: [
+                          Text(
+                            "2:13",
+                            style: context.textTheme.subtitle1,
+                          ),
+                          const Spacer(),
+                          Text(
+                            "4:10",
+                            style: context.textTheme.subtitle1,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 30,
+                        right: 30,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          MButtonForSong(
+                            icon: Icons.arrow_back_ios,
+                          ),
+                          const SizedBox(width: 20),
+                          MButtonForSong(
+                            icon: Icons.play_arrow,
+                            boxColor: true,
+                          ),
+                          SizedBox(width: 20),
+                          MButtonForSong(icon: Icons.arrow_forward_ios)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
