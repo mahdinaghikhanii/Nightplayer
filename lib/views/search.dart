0038e216c7f7ebb9389915bc7988cdfe7f7a6b18
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-22 21:50:16 +0430

    finishid desing in Search Bars

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -2,19 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:nightplayer/module/extention.dart';
 
 class Search extends SearchDelegate<String> {
-  Widget build(BuildContext context) {
-    return Container();
-  }
-
   @override
-  List<Widget>? buildActions(BuildContext context) {
-    IconButton(
-        onPressed: () {
-          query = '';
-        },
-        icon: const Icon(Icons.clear));
-    return null;
-  }
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+            onPressed: () {
+              query = '';
+            },
+            icon: const Icon(
+              Icons.clear,
+            ))
+      ];
 
   @override
   Widget? buildLeading(BuildContext context) {
@@ -28,13 +25,31 @@ class Search extends SearchDelegate<String> {
 
   @override
   Widget buildResults(BuildContext context) {
-    throw Container(
-      color: Colors.black,
-    );
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Icon(Icons.confirmation_num, size: 120),
+        const SizedBox(
+          height: 30,
+        ),
+        Text(query)
+      ],
+    ));
   }
 
+  // baraye respone serach
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: listSearch.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: ListTile(
+              title: listSearch[index],
+            ),
+          );
+        });
+    ;
   }
 }
