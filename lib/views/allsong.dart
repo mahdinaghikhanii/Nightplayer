055db39f851f28d2fcd0f637cf52db6d523f8f73
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-26 21:00:02 +0430

    add appbar in all song

--- lib/views/allsong.dart
+++ lib/views/allsong.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:nightplayer/module/extention.dart';
 import 'package:nightplayer/module/widgets.dart';
 import 'package:nightplayer/views/playorstopsong.dart';
+import 'package:nightplayer/views/search.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 import '../module/constans.dart';
@@ -13,125 +14,163 @@ class AllSong extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final OnAudioQuery audioQuery = OnAudioQuery();
-    return Column(
-      children: [
-        Expanded(
-            child: FutureBuilder<List<SongModel>>(
-                future: audioQuery.querySongs(
-                  sortType: null,
-                  orderType: OrderType.ASC_OR_SMALLER,
-                  uriType: UriType.EXTERNAL,
-                  ignoreCase: true,
-                ),
-                builder: (context, iteam) {
-                  if (iteam.data == null) {
-                    return const Center(child: CupertinoActivityIndicator());
-                  }
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Constans.kdefultAppColor,
+        automaticallyImplyLeading: false,
+        leadingWidth: 0,
+        elevation: 0,
+        titleSpacing: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: Constans.kdefualtAppPading),
+            child: IconButton(
+                onPressed: () {
+                  showSearch(
+                    context: context,
+                    delegate: Search(),
+                    query: "",
+                  );
+                },
+                icon: const Icon(
+                  CupertinoIcons.search,
+                  size: 26,
+                )),
+          )
+        ],
+        title: Padding(
+          padding: const EdgeInsets.symmetric(
+              horizontal: Constans.kdefualtAppPading),
+          child: Text(
+            'NightPlayer',
+            style: context.textTheme.subtitle1!.copyWith(fontSize: 22),
+          ),
+        ),
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: FutureBuilder<List<SongModel>>(
+                  future: audioQuery.querySongs(
+                    sortType: null,
+                    orderType: OrderType.ASC_OR_SMALLER,
+                    uriType: UriType.EXTERNAL,
+                    ignoreCase: true,
+                  ),
+                  builder: (context, iteam) {
+                    if (iteam.data == null) {
+                      return const Center(child: CupertinoActivityIndicator());
+                    }
 
-                  if (iteam.data!.isEmpty) {
-                    return const MNotFound();
-                  }
+                    if (iteam.data!.isEmpty) {
+                      return const MNotFound();
+                    }
 
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 2),
-                    child: ListView.builder(
-                        physics: const ScrollPhysics(),
-                        itemCount: iteam.data!.length,
-                        shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 18),
-                            width: double.infinity,
-                            height: 68,
-                            margin: const EdgeInsets.only(top: 10, bottom: 10),
-                            child: InkWell(
-                              borderRadius: BorderRadius.circular(20),
-                              onTap: () async {
-                                context.audioCuibt.showMusicNotification(
-                                    iteam.data![index].id.toString(),
-                                    // 'assets/icon/song248.png',
-                                    iteam.data![index].title,
-                                    iteam.data![index].artist ?? "No Artist",
-                                    index);
-                                context.nextPage(PlayOrStopSong(
-                                    songModel: iteam.data![index]));
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.only(top: 0),
-                                child: Row(
-                                  children: [
-                                    Hero(
-                                      tag: iteam.data![index].id,
-                                      child: QueryArtworkWidget(
-                                          nullArtworkWidget: Image.asset(
-                                            'assets/icon/song248.png',
-                                            width: 65,
-                                            height: 65,
-                                            fit: BoxFit.fill,
-                                          ),
-                                          artworkBorder:
-                                              BorderRadius.circular(10),
-                                          artworkWidth: 65,
-                                          artworkHeight: 65,
-                                          id: iteam.data![index].id,
-                                          type: ArtworkType.AUDIO),
-                                    ),
-                                    const SizedBox(width: 20),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        const SizedBox(height: 10),
-                                        Expanded(
-                                          child: SizedBox(
-                                            width: 180,
-                                            height: 24,
-                                            child: Text(
-                                              iteam.data![index].title,
-                                              overflow: TextOverflow.fade,
-                                              style: context
-                                                  .textTheme.subtitle1!
-                                                  .copyWith(fontSize: 16),
-                                              maxLines: 1,
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2),
+                      child: ListView.builder(
+                          physics: const ScrollPhysics(),
+                          itemCount: iteam.data!.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 18),
+                              width: double.infinity,
+                              height: 68,
+                              margin:
+                                  const EdgeInsets.only(top: 10, bottom: 10),
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(20),
+                                onTap: () async {
+                                  context.audioCuibt.showMusicNotification(
+                                      iteam.data![index].id.toString(),
+                                      // 'assets/icon/song248.png',
+                                      iteam.data![index].title,
+                                      iteam.data![index].artist ?? "No Artist",
+                                      index);
+                                  context.nextPage(PlayOrStopSong(
+                                      songModel: iteam.data![index]));
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.only(top: 0),
+                                  child: Row(
+                                    children: [
+                                      Hero(
+                                        tag: iteam.data![index].id,
+                                        child: QueryArtworkWidget(
+                                            nullArtworkWidget: Image.asset(
+                                              'assets/icon/song248.png',
+                                              width: 65,
+                                              height: 65,
+                                              fit: BoxFit.fill,
+                                            ),
+                                            artworkBorder:
+                                                BorderRadius.circular(10),
+                                            artworkWidth: 65,
+                                            artworkHeight: 65,
+                                            id: iteam.data![index].id,
+                                            type: ArtworkType.AUDIO),
+                                      ),
+                                      const SizedBox(width: 20),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          const SizedBox(height: 10),
+                                          Expanded(
+                                            child: SizedBox(
+                                              width: 180,
+                                              height: 24,
+                                              child: Text(
+                                                iteam.data![index].title,
+                                                overflow: TextOverflow.fade,
+                                                style: context
+                                                    .textTheme.subtitle1!
+                                                    .copyWith(fontSize: 16),
+                                                maxLines: 1,
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                        const SizedBox(height: 10),
-                                        Expanded(
-                                          child: SizedBox(
-                                            width: 180,
-                                            height: 24,
-                                            child: Text(
-                                                maxLines: 1,
-                                                softWrap: false,
-                                                overflow: TextOverflow.clip,
-                                                iteam.data![index].artist ??
-                                                    "No Artist",
-                                                style: context
-                                                    .textTheme.subtitle1),
+                                          const SizedBox(height: 10),
+                                          Expanded(
+                                            child: SizedBox(
+                                              width: 180,
+                                              height: 24,
+                                              child: Text(
+                                                  maxLines: 1,
+                                                  softWrap: false,
+                                                  overflow: TextOverflow.clip,
+                                                  iteam.data![index].artist ??
+                                                      "No Artist",
+                                                  style: context
+                                                      .textTheme.subtitle1),
+                                            ),
                                           ),
-                                        ),
-                                      ],
-                                    ),
-                                    const Spacer(),
-                                    IconButton(
-                                        onPressed: (() {}),
-                                        icon: Icon(
-                                          Icons.more_horiz,
-                                          size: 35,
-                                          color: Constans.kwhite,
-                                        ))
-                                  ],
+                                        ],
+                                      ),
+                                      const Spacer(),
+                                      IconButton(
+                                          onPressed: (() {}),
+                                          icon: Icon(
+                                            Icons.more_horiz,
+                                            size: 35,
+                                            color: Constans.kwhite,
+                                          ))
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          );
-                        }),
-                  );
-                }))
-      ],
+                            );
+                          }),
+                    );
+                  }))
+        ],
+      ),
     );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,16 +1,15 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/audio_bloc/audio_state.dart';
 import '../bloc/btnnavigation_bloc/btmnavigation_cubit.dart';
 import '../bloc/btnnavigation_bloc/btmnavigation_state.dart';
-import '../module/constans.dart';
-import '../module/extention.dart';
 import '../module/navbar_items.dart';
 import '../module/widgets.dart';
+import 'album.dart';
 import 'allsong.dart';
-import 'search.dart';
+import 'favorite.dart';
+import 'settings.dart';
 
 class Home extends StatelessWidget {
   final AudioState stateaudio;
@@ -21,45 +20,18 @@ class Home extends StatelessWidget {
     return Scaffold(
         extendBody: true,
         bottomNavigationBar: const MBottomNavigation(),
-        appBar: AppBar(
-          backgroundColor: Constans.kdefultAppColor,
-          automaticallyImplyLeading: false,
-          leadingWidth: 0,
-          elevation: 0,
-          titleSpacing: 0,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: Constans.kdefualtAppPading),
-              child: IconButton(
-                  onPressed: () {
-                    showSearch(
-                      context: context,
-                      delegate: Search(),
-                      query: "",
-                    );
-                  },
-                  icon: const Icon(
-                    CupertinoIcons.search,
-                    size: 26,
-                  )),
-            )
-          ],
-          title: Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: Constans.kdefualtAppPading),
-            child: Text(
-              'NightPlayer',
-              style: context.textTheme.subtitle1!.copyWith(fontSize: 22),
-            ),
-          ),
-          centerTitle: false,
-        ),
         body: BlocBuilder<BtmNavigationCubit, BtmNavigationState>(
             builder: ((context, state) {
           if (state.navbarItem == NavbarItem.allsong) {
             return const AllSong();
+          } else if (state.navbarItem == NavbarItem.album) {
+            return const Album();
+          } else if (state.navbarItem == NavbarItem.favorite) {
+            return const Favorite();
+          } else if (state.navbarItem == NavbarItem.settings) {
+            return const Settings();
           }
+
           return Container();
         })));
   }
