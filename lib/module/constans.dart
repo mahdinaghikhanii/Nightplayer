2b29c302984cc5a83ed4d37f66356a56073269e4
Mahdinaghikhani <mahdinkh7@gmail.com>
2022-06-27 11:34:26 +0430

    change style text in listView home

--- lib/module/constans.dart
+++ lib/module/constans.dart
@@ -3,7 +3,7 @@ import 'package:flutter_hex_color/flutter_hex_color.dart';
 
 class Constans {
   static const double kdefualtSizeIcon = 25;
-  static const double kdefualtAppPading = 20;
+  static const double kdefualtAppPading = 15;
   static Color kdefultAppColor = HexColor('#1C223A');
   static Color navyblueshade2 = HexColor('#1E233E');
   static Color kwhite = Colors.white;
--- lib/views/allsong.dart
+++ lib/views/allsong.dart
@@ -15,6 +15,12 @@ class AllSong extends StatelessWidget {
   Widget build(BuildContext context) {
     final OnAudioQuery audioQuery = OnAudioQuery();
     return Scaffold(
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(bottom: 55),
+        width: double.infinity,
+        height: 40,
+        color: Colors.red,
+      ),
       appBar: AppBar(
         backgroundColor: Constans.kdefultAppColor,
         automaticallyImplyLeading: false,
@@ -68,107 +74,106 @@ class AllSong extends StatelessWidget {
                       return const MNotFound();
                     }
 
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 2),
-                      child: ListView.builder(
-                          physics: const ScrollPhysics(),
-                          itemCount: iteam.data!.length,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 18),
-                              width: double.infinity,
-                              height: 68,
-                              margin:
-                                  const EdgeInsets.only(top: 10, bottom: 10),
-                              child: InkWell(
-                                borderRadius: BorderRadius.circular(20),
-                                onTap: () async {
-                                  context.audioCuibt.showMusicNotification(
-                                      iteam.data![index].id.toString(),
-                                      // 'assets/icon/song248.png',
-                                      iteam.data![index].title,
-                                      iteam.data![index].artist ?? "No Artist",
-                                      index);
-                                  context.nextPage(PlayOrStopSong(
-                                      songModel: iteam.data![index]));
-                                },
-                                child: Padding(
-                                  padding: const EdgeInsets.only(top: 0),
-                                  child: Row(
+                    return ListView.builder(
+                        physics: const ScrollPhysics(),
+                        itemCount: iteam.data!.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 0),
+                            width: double.infinity,
+                            height: 70,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(20),
+                              onTap: () async {
+                                context.audioCuibt.showMusicNotification(
+                                    iteam.data![index].id.toString(),
+                                    // 'assets/icon/song248.png',
+                                    iteam.data![index].title,
+                                    iteam.data![index].artist ?? "No Artist",
+                                    index);
+                                context.nextPage(PlayOrStopSong(
+                                    songModel: iteam.data![index]));
+                              },
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 5),
+                                    child: Hero(
+                                      tag: iteam.data![index].id,
+                                      child: QueryArtworkWidget(
+                                          nullArtworkWidget: Image.asset(
+                                            'assets/icon/song248.png',
+                                            width: 60,
+                                            height: 60,
+                                            fit: BoxFit.fill,
+                                          ),
+                                          artworkBorder:
+                                              BorderRadius.circular(10),
+                                          artworkWidth: 60,
+                                          artworkHeight: 60,
+                                          id: iteam.data![index].id,
+                                          type: ArtworkType.AUDIO),
+                                    ),
+                                  ),
+                                  const SizedBox(width: 15),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisSize: MainAxisSize.min,
                                     children: [
-                                      Hero(
-                                        tag: iteam.data![index].id,
-                                        child: QueryArtworkWidget(
-                                            nullArtworkWidget: Image.asset(
-                                              'assets/icon/song248.png',
-                                              width: 65,
-                                              height: 65,
-                                              fit: BoxFit.fill,
-                                            ),
-                                            artworkBorder:
-                                                BorderRadius.circular(10),
-                                            artworkWidth: 65,
-                                            artworkHeight: 65,
-                                            id: iteam.data![index].id,
-                                            type: ArtworkType.AUDIO),
-                                      ),
-                                      const SizedBox(width: 20),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          const SizedBox(height: 10),
-                                          Expanded(
-                                            child: SizedBox(
-                                              width: 180,
-                                              height: 24,
-                                              child: Text(
-                                                iteam.data![index].title,
-                                                overflow: TextOverflow.fade,
-                                                style: context
-                                                    .textTheme.subtitle1!
-                                                    .copyWith(fontSize: 16),
-                                                maxLines: 1,
-                                              ),
-                                            ),
+                                      const SizedBox(height: 10),
+                                      Expanded(
+                                        child: SizedBox(
+                                          width: 200,
+                                          height: 24,
+                                          child: Text(
+                                            iteam.data![index].title,
+                                            overflow: TextOverflow.fade,
+                                            style: context.textTheme.subtitle1!
+                                                .copyWith(fontSize: 16),
+                                            maxLines: 1,
                                           ),
-                                          const SizedBox(height: 10),
-                                          Expanded(
-                                            child: SizedBox(
-                                              width: 180,
-                                              height: 24,
-                                              child: Text(
-                                                  maxLines: 1,
-                                                  softWrap: false,
-                                                  overflow: TextOverflow.clip,
-                                                  iteam.data![index].artist ??
-                                                      "No Artist",
-                                                  style: context
-                                                      .textTheme.subtitle1),
-                                            ),
-                                          ),
-                                        ],
+                                        ),
                                       ),
-                                      const Spacer(),
-                                      IconButton(
-                                          onPressed: (() {}),
-                                          icon: Icon(
-                                            Icons.more_horiz,
-                                            size: 35,
-                                            color: Constans.kwhite,
-                                          ))
+                                      const SizedBox(height: 0),
+                                      Expanded(
+                                        child: SizedBox(
+                                          width: 180,
+                                          height: 24,
+                                          child: Text(
+                                              maxLines: 1,
+                                              softWrap: false,
+                                              overflow: TextOverflow.clip,
+                                              iteam.data![index].artist ??
+                                                  "No Artist",
+                                              style: context
+                                                  .textTheme.subtitle1!
+                                                  .copyWith(fontSize: 14)),
+                                        ),
+                                      ),
                                     ],
                                   ),
-                                ),
+                                  const Spacer(),
+                                  IconButton(
+                                      onPressed: (() {}),
+                                      icon: Icon(
+                                        Icons.more_horiz,
+                                        size: 35,
+                                        color: Constans.kwhite,
+                                      ))
+                                ],
                               ),
-                            );
-                          }),
-                    );
-                  }))
+                            ),
+                          );
+                        });
+                  })),
+          Container(
+            width: double.infinity,
+            height: 20,
+            color: Colors.red,
+          )
         ],
       ),
     );
